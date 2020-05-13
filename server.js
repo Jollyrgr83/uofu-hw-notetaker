18c99562c4de813556811ad450dc25a8cc99bb8a
Jollyrgr83 <jasongmangin@gmail.com>
2020-05-13 15:01:50 -0600

    uploading to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var fs = require("fs");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // data parsing through express app
 app.use(express.urlencoded({ extended: true }));
